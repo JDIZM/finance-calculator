e6dfcbf0b98ed92574dc381e0938d049d3305893
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2023-09-08 18:53:40 +0100

    feat: example navbar component

---
+++ src/components/layout/navbar/NavBar.spec.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+
+import { mount, VueWrapper } from '@vue/test-utils'
+import NavBar from './NavBar.vue'
+
+describe('NavBar', () => {
+  let wrapper: VueWrapper<any>
+
+  const nav = () => wrapper.find('nav')
+  const logo = () => wrapper.find('svg')
+
+  beforeEach(() => {
+    wrapper = mount(NavBar, {
+      global: {
+        stubs: ['router-link']
+      }
+    })
+  })
+  it('renders properly', () => {
+    console.log(wrapper.html())
+    expect(wrapper.exists()).toBe(true)
+    expect(logo().exists()).toBe(true)
+    expect(nav().exists()).toBe(true)
+  })
+})
