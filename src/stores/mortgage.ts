46ec91417f73ea226e00e688c39ace500fd40aeb
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-01-31 14:49:24 +0000

    feat: mortgage store

---
+++ src/stores/mortgage.ts
@@ -0,0 +1,47 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import type {
+  InterestOnlyMortgageResult,
+  MortgageOptions,
+  MortgageResult,
+  MortgageType
+} from '@jdizm/finance-calculator/types/calculator'
+
+export const useMortgageStore = defineStore('mortgage', () => {
+  // Setup stores
+  // https://pinia.vuejs.org/core-concepts/#Setup-Stores
+
+  // State
+  const options = ref<MortgageOptions | null>(null)
+  const mortgagetype = ref<MortgageType>('repayment')
+  const results = ref<MortgageResult | InterestOnlyMortgageResult | null>(null)
+  const error = ref<Error | null>(null)
+
+  // Actions
+  function setOptions(payload: MortgageOptions) {
+    options.value = payload
+  }
+
+  function setMortgageType(payload: MortgageType) {
+    mortgagetype.value = payload
+  }
+
+  function setResults(payload: MortgageResult | InterestOnlyMortgageResult) {
+    results.value = payload
+  }
+
+  function setError(err: Error | null) {
+    error.value = err
+  }
+
+  return {
+    options,
+    mortgagetype,
+    results,
+    setOptions,
+    setMortgageType,
+    setResults,
+    error,
+    setError
+  }
+})
