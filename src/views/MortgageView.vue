a549890e3f6c0e12a1baf67d9992df9b02b54cd6
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-01-31 14:57:21 +0000

    feat: mortgage view

--- src/views/MortgageView.vue
+++ src/views/MortgageView.vue
@@ -1,24 +1,39 @@
 <template>
   <main>
-    <h2>Mortgage Calculator</h2>
+    <h1>Mortgage Calculator</h1>
     <p>a simple mortgage calculator</p>
-    <h4>Coming soon...</h4>
-    <FinanceOptions :options="financeOptions" />
+    <MortgageCalculator />
+    <div class="results" v-if="store.results">
+      <h2>Mortgage Results</h2>
+      <p>
+        Your monthly mortgage repayment for a <strong>{{ store.mortgagetype }}</strong> mortgage
+        would be
+        <span v-if="store.mortgagetype === 'repayment' && 'monthlyRepayment' in store.results">
+          <strong>{{ formattedNumber(store.results.monthlyRepayment) }}</strong>
+        </span>
+        <span v-if="store.mortgagetype === 'interestOnly' && 'interestPayments' in store.results">
+          <strong>{{ formattedNumber(store.results.interestPayments.monthly) }}</strong>
+        </span>
+      </p>
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import FinanceOptions from '@/components/FinanceOptions.vue'
-import { ref } from 'vue'
+import MortgageCalculator from '@/components/calculator/mortgage/MortgageCalculator.vue'
+import { formattedNumber } from '@/helpers/numbers'
+import { useMortgageStore } from '@/stores/mortgage'
 
-const financeOptions = ref([
-  {
-    text: 'Compound Interest',
-    value: 'compound-interest'
-  },
-  {
-    text: 'Mortgage',
-    value: 'mortgage'
+const store = useMortgageStore()
+
+// Subscription to store actions
+store.$onAction((action) => {
+  console.log('action', action)
+  if (action.name === 'setOptions') {
+    console.log('setOptions', action)
   }
-])
+  if (action.name === 'setResults') {
+    console.log('setResults', action)
+  }
+})
 </script>
