128c83d9b955c4c1b987b5112442ea453680f291
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-02-04 08:19:50 +0000

    test: compound interest form

--- src/components/calculator/compound-interest/form/CompoundInterestForm.spec.ts
+++ src/components/calculator/compound-interest/form/CompoundInterestForm.spec.ts
@@ -1,15 +1,36 @@
 import { describe, it, expect, beforeEach } from 'vitest'
-import { mount, VueWrapper } from '@vue/test-utils'
-import CompoundInterestForm from './CompoundInterestForm.vue'
+import { render, screen, type RenderResult, fireEvent } from '@testing-library/vue'
+import { composeStories } from '@storybook/testing-vue3'
+import * as stories from './CompoundInterestForm.stories'
+
+const { Default } = composeStories(stories)
 
 describe('CompoundInterestForm', () => {
-  let wrapper: VueWrapper<any>
+  let wrapper: RenderResult
+  const { args } = Default
 
   beforeEach(() => {
-    wrapper = mount(CompoundInterestForm)
+    wrapper = render(Default())
+
+    return () => {
+      wrapper.unmount()
+    }
   })
 
-  it('renders properly', () => {
-    expect(wrapper.exists()).toBe(true)
+  it('renders debtRepaymentOptions when checkbox is clicked.', async () => {
+    const debtRepaymentCheckbox = screen.getByLabelText('Is the principal borrowed?')
+
+    await fireEvent.click(debtRepaymentCheckbox)
+
+    const emittedEvent = wrapper.emitted('change')
+
+    const interestRateInput = screen.getByLabelText('Interest Rate of the loaned principal')
+    const radioButtons = screen.getAllByRole('radio')
+
+    expect(emittedEvent.length).toBe(1)
+    expect(interestRateInput).not.toBeNull()
+    expect(radioButtons.length).toBe(2)
+    expect(radioButtons[0].outerHTML).toContain('value="interestOnly"')
+    expect(radioButtons[1].outerHTML).toContain('value="repayment"')
   })
 })
